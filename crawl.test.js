90acc82e790f6dac66eecd332c783fb7dcf98f1b
hemang-h <hemangcertificate@gmail.com>
2023-02-26 23:23:53 +0530

    Added more functions to normaLizeURL to strip http, captials, trailiing slashes

--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,28 @@
 const {normalizeURL} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
-test('normalizeURL', () => {
+test('normalizeURL strip protocol', () => {
+    const input = 'https://google.com'
+    const actual = normalizeURL(input)
+    const expected = 'google.com'
+    expect(actual).toEqual(expected)
+}) 
+
+test('normalizeURL capitals', () => {
+    const input = 'https://GOOGLE.com'
+    const actual = normalizeURL(input)
+    const expected = 'google.com'
+    expect(actual).toEqual(expected)
+}) 
+
+test('normalizeURL trailing Slashes', () => {
+    const input = 'https://google.com'
+    const actual = normalizeURL(input)
+    const expected = 'google.com'
+    expect(actual).toEqual(expected)
+}) 
+
+test('normalizeURL strip http', () => {
     const input = 'https://google.com'
     const actual = normalizeURL(input)
     const expected = 'google.com'
