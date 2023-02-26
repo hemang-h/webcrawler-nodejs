36d9706f56e9e2582ef75c5452c7e30c9b875905
hemang-h <hemangcertificate@gmail.com>
2023-02-27 00:19:57 +0530

    Intial setup for main

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+function main() {
+    if(process.argv.length < 3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if(process.argv.length > 3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+   const baseURL = process.argv[2]
+    console.log(`Starting crawler of ${baseURL}`)
+}
+
+main()
